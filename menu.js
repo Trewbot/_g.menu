16c54d3e03bea73f8d7a0d5bdacb0d8ab6bd19fc
Trewbot <trewbot@phene.co>
2015-10-16 15:20:19 -0500

    Added changes.md

--- menu.js
+++ menu.js
@@ -1,47 +1,7 @@
 /*
  *	Graphene Context Menu
  *	Written by Trewbot and Savoron
- *	Dec 26, 2014
- *
- *	Version:				Date:				Description:
- *		v0.0.0.0000				Oct 21, 2014		Script separated from Graphene/scripts.js
- *		v0.0.1.0001				Oct 21, 2014		Refactored
- *		v0.0.1.0002				Oct 21, 2014		Added Permalink and New Tab options when link targeted
- *		v0.0.1.0003				Oct 21, 2014		Added New Window option (opens popup window) when link targeted
- *		v0.0.1.0004				Oct 21, 2014		Blocked Permalink option when targeted link is external
- *		v0.0.1.0005				Oct 21, 2014		Added Log Element and Log Properties options when console is opened (determined by outerHeight/Width > innerHeight/Width - 150)
- *		v0.0.1.0005.5			Dec 25, 2014		Added quick fix for Graphene test build w0.4.0.0392
- *		v0.1.0.0006				Dec 25, 2014		Rewrite based on Graphene w0.4.*'s object-orientation as well as fixing compatability with non-Graphene sites
- *		v0.1.0.0007				Dec 26, 2014		Refactoring
- *		v0.2.0.0008				Dec 26, 2014		Moved styling into scripts
- *		v0.3.0.0009				Dec 26, 2014		Added positioning to keep menu visible
- *		v0.3.0.0009				Dec 26, 2014		Removed developer options
- *		v0.3.0.0010				Dec 26, 2014		Fixed parentAnchor erroring on null
- *		v0.3.0.0011				Dec 29, 2014		Removed unnecessary quotation marks from object keys. Used automatic formatting on source code for readability.
- *		v0.3.0.0012				Dec 29, 2014		Fixed Scroll to Top button.
- *		v0.4.0.0013				Jan 05, 2015		Added text selection check
- *		v0.4.0.0014				Jan 05, 2015		Fixed error on right-clicking context menu.
- *		v0.4.0.0015				Jan 05, 2015		Added Flash to copy text to clipboard.
- *		v0.4.1.0016				Jan 05, 2015		Added Search Google option for selected text
- *		v0.4.2.0017				Jan 06, 2015		Added copy link locations
- *		v0.4.2.0018				Jan 06, 2015		Fixed copy link location for blank links
- *		v0.4.2.0019				Jan 06, 2015		Added link location copying back in for blank links, switching the adress with the current loction
- *		v0.4.2.0019.5			Jan 15, 2015		Added quick fix for Graphene test build w0.4.0.0419
- *		v0.4.3.0020				Feb 21, 2015		Added video element source copy
- *		v0.4.3.0021				Feb 21, 2015		Added video element pause/play buttons
- *		v0.4.3.0022				Feb 21, 2015		Added video element mute/unmute buttons
- *		v0.4.3.0023				Feb 21, 2015		Fixed menu not closing after pressing copy buttons
- *		v0.4.3.0024				Feb 21, 2015		Added video element source download button
- *		v0.4.3.0025				Feb 21, 2015		Added link download button
- *		v0.4.3.0026				Feb 21, 2015		Fixed scroll to position function (__stp__)
- *		v0.4.3.0027				Feb 21, 2015		Removed download buttons on Firefox (due to non-supporting behavior of CORS download attributes)
- *		v0.4.3.0028				Feb 22, 2015		Fixed script set options bottom border not working
- *		v0.4.3.0029				Feb 22, 2015		Fixed script options not working when isOpen
- *		v0.4.3.0030				Feb 22, 2015		Renamed _g.m to _g/m and _g.context to _g.menu
- *		v0.4.3.0031				Feb 22, 2015		Fixed scrolling not removing context menu
- *		v0.4.3.0032				Feb 22, 2015		Added context menu removal on window blur
- *		v0.4.3.0033				Feb 23, 2015		Removed anchor element permalink option 
- *		v0.4.3.0034				Feb 23, 2015		Removed anchor element new window option (does not work as expected)
+ *	Feb 23, 2015
  */
 
 Element.prototype.remove = function () {
